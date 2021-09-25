bf4fd7dd71cc75f16ff2162f6a565a2669ca43ef
Willian Oliveira <willian.oliveira@arizona.global>
2021-09-25 13:21:50 -0300

    Atualiza registros (dados.json)

--- updateData.sh
+++ updateData.sh
@@ -1,4 +1,4 @@
-wget -O /home/arizona/Estudos/Pets/aircraft-registration/data/dados.json "https://sistemas.anac.gov.br/dadosabertos/Aeronaves/RAB/dados_aeronaves.json"
+wget -O ./data/dados.json "https://sistemas.anac.gov.br/dadosabertos/Aeronaves/RAB/dados_aeronaves.json"
 git add .
 git commit -m "Atualiza registros (dados.json)"
 git push
